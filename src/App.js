3485dca4ea303c114bbfc23505b1c0ea88716650
hyoj <chohyojoy@gmail.com>
2019-08-04 20:54:54 +0900

    Initial commit
    React JS Fundamentals 7 / 20

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie'
+
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/51Q7Psa2ufL.jpg"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/41VXPrZfDXL.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://cdn.shopify.com/s/files/1/0747/3829/products/HP2602_b2e4c3a4-eeca-4fc4-afde-ef023c4fb31d_1024x1024.jpg?v=1515503915"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://starwarsblog.starwars.com/wp-content/uploads/2015/10/tfa_poster_wide_header-1536x864-959818851016.jpg"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movies.map(movie => {
+        return <Movie title={movie.title} poster={movie.poster} />
+      })}
     </div>
   );
 }
